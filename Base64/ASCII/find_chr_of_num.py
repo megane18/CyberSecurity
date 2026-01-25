54767f5196f2d3ff28da5e9699ce17b4d9447a1e
megane18 <meganealexis12@gmail.com>
2026-01-24 19:39:20 -0500

    Updated ASCII folder

--- Base64/ASCII/find_chr_of_num.py
+++ Base64/ASCII/find_chr_of_num.py
@@ -1,3 +1,4 @@
+import base64
 def find_chr_of_ord(s):
 
     result = []
@@ -7,11 +8,15 @@ def find_chr_of_ord(s):
     return result
 
 
-s=[77,101,103,97,110,101]
-print(''.join(find_chr_of_ord(s)))
+# s=[77,101,103,97,110,101]
+# print(''.join(find_chr_of_ord(s)))
 
 
-
+# convert the following int to message
+s=11515195063862318899931685488813747395775516287289682636499965282714637259206269
+from Crypto.Util.number import long_to_bytes
+message = long_to_bytes(s)
+print(message)
 
 
 
